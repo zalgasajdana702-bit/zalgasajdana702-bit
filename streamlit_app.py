458cc7a71fd39bfb054552e9b93bcf9db5021f8a
zalgasajdana702-bit <zalgasajdana702@gmail.com>
2026-04-09 11:58:48 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if st.button("Сгенерировать"):
     else:
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel("models/gemini-1.0-pro")
+            model = genai.GenerativeModel('gemini-1.5-flash')
 
             prompt = f"""
             Улучши текст в стиле: {tone}.
